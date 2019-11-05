63fcf83ee920420bb47b24212fada95dd79ba074
Raju Singh <raju.singh41@gmail.com>
2019-11-06 00:16:19 +0530

    Add project files.

---
+++ SchedulerConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSchedulerLog();
+        }
+
+        private static void WriteSchedulerLog()
+        {
+            var logFilePath = Path.GetFullPath(@"D:\ConsoleSchedulerDemo\")+ DateTime.Now.ToString("MM_dd_yyyy_HH_mm")+"_log.txt";
+            try
+            {
+                if(!File.Exists(logFilePath))
+                {
+                    File.Create(logFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                var errorFilePath = @"D:\ConsoleSchedulerDemo";
+                File.AppendAllText(errorFilePath, Environment.NewLine + ex.Message);
+            }
+        }
+    }
+}
